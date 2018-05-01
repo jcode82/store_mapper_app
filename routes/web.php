fca5a551ddba4b82ca2045e86bbda23a86198475
Juan Flores <jflifestyles@gmail.com>
2018-05-01 18:51:26 -0400

    add markers to route, fix DB connection, clean up view

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,36 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $config['center'] = 'carbuckets, Miami, FL';
+    $config['zoom'] = '16';
+    $config['map_height'] = '500px';
+    $config['scrollwheel'] = false;
+
+    GMaps::initialize($config);
+
+    //add marker
+    $marker['position'] = 'carbuckets, Miami, FL';
+    $marker['infowindow_content'] = 'Car Buckets';
+    GMaps::add_marker($marker);
+
+    //add marker
+    $marker['position'] = 'braman miami, Miami, FL';
+    $marker['infowindow_content'] = 'Braman Miami';
+    GMaps::add_marker($marker);
+
+    //add marker
+    $marker['position'] = 'walt grace vintage, Miami, FL';
+    $marker['infowindow_content'] = 'Walt Grace Vintagei';
+    GMaps::add_marker($marker);
+
+     //add marker
+     $marker['position'] = 'hi tech motors, Miami, FL';
+     $marker['infowindow_content'] = 'Hi Tech Motors';
+     $marker['icon'] = 'http://maps.google.com/mapfiles/ms/micons/green-dot.png';
+     GMaps::add_marker($marker);
+
+    $map = GMaps::create_map();
+
+    return view('welcome')->with('maps', $map);
 });
